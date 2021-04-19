424de03c3917360b0fde55268d2d6bbabed83918
gaoyanglol <gaoyanglol@hotmail.com>
2021-04-19 17:14:34 +0800

    bloodLcq question list

--- pages/bloodLcq/bloodLcq.js
+++ pages/bloodLcq/bloodLcq.js
@@ -4,8 +4,8 @@ Page({
     vertical: false,
     autoplay: false,
     interval: 2000,
-    duration: 0,
-    current: 1,
+    duration: 200,
+    current: 0,
     items: [
       { 
         value: "1", 
@@ -18,7 +18,8 @@ Page({
           {value: "5", title: "E", name: "很少会"},
           {value: "6", title: "F", name: "几乎不会"},
           {value: "7", title: "G", name: "一点也不会"}
-        ]
+        ],
+        chosen: ""
       },
       { 
         value: "2", 
@@ -31,7 +32,8 @@ Page({
           {value: "5", title: "E", name: "偶尔会"},
           {value: "6", title: "F", name: "极少会"},
           {value: "7", title: "G", name: "从来不会"}
-        ]
+        ],
+        chosen: ""
       },
       { 
         value: "3", 
@@ -44,7 +46,8 @@ Page({
           {value: "5", title: "E", name: "很少会"},
           {value: "6", title: "F", name: "几乎不会"},
           {value: "7", title: "G", name: "一点也不会"}
-        ]
+        ],
+        chosen: ""
       },
       { 
         value: "4", 
@@ -57,36 +60,269 @@ Page({
           {value: "5", title: "E", name: "常常能"},
           {value: "6", title: "F", name: "多数时间能"},
           {value: "7", title: "G", name: "一直都能"}
-        ]
+        ],
+        chosen: ""
       },
-    ]
+      { 
+        value: "5", 
+        question: "近两周来，咳嗽会让您觉得尴尬吗？", 
+        answer: [
+          {value: "1", title: "A", name: "一直都会"},
+          {value: "2", title: "B", name: "大多数时间会"},
+          {value: "3", title: "C", name: "时常会"},
+          {value: "4", title: "D", name: "有时会"},
+          {value: "5", title: "E", name: "很少会"},
+          {value: "6", title: "F", name: "几乎不会"},
+          {value: "7", title: "G", name: "一点也不会"}
+        ],
+        chosen: ""
+      },
+      { 
+        value: "6", 
+        question: "近两周来，咳嗽会让您焦虑不安吗？", 
+        answer: [
+          {value: "1", title: "A", name: "一直都会"},
+          {value: "2", title: "B", name: "大多数时间会"},
+          {value: "3", title: "C", name: "时常会"},
+          {value: "4", title: "D", name: "有时会"},
+          {value: "5", title: "E", name: "很少会"},
+          {value: "6", title: "F", name: "几乎不会"},
+          {value: "7", title: "G", name: "一点也不会"}
+        ],
+        chosen: ""
+      },
+      { 
+        value: "7", 
+        question: "近两周来，咳嗽会影响您的工作或其他日常事务吗？", 
+        answer: [
+          {value: "1", title: "A", name: "一直都会"},
+          {value: "2", title: "B", name: "大多数时间会"},
+          {value: "3", title: "C", name: "时常会"},
+          {value: "4", title: "D", name: "有时会"},
+          {value: "5", title: "E", name: "很少会"},
+          {value: "6", title: "F", name: "几乎不会"},
+          {value: "7", title: "G", name: "一点也不会"}
+        ],
+        chosen: ""
+      },
+      { 
+        value: "8", 
+        question: "近两周来，咳嗽会影响您的整个娱乐生活吗？", 
+        answer: [
+          {value: "1", title: "A", name: "一直都会"},
+          {value: "2", title: "B", name: "大多数时间会"},
+          {value: "3", title: "C", name: "时常会"},
+          {value: "4", title: "D", name: "有时会"},
+          {value: "5", title: "E", name: "很少会"},
+          {value: "6", title: "F", name: "几乎不会"},
+          {value: "7", title: "G", name: "一点也不会"}
+        ],
+        chosen: ""
+      },
+      { 
+        value: "9", 
+        question: "近两周来，接触油漆油烟会让您咳嗽吗？", 
+        answer: [
+          {value: "1", title: "A", name: "一直都会"},
+          {value: "2", title: "B", name: "大多数时间会"},
+          {value: "3", title: "C", name: "时常会"},
+          {value: "4", title: "D", name: "有时会"},
+          {value: "5", title: "E", name: "很少会"},
+          {value: "6", title: "F", name: "几乎不会"},
+          {value: "7", title: "G", name: "一点也不会"}
+        ],
+        chosen: ""
+      },
+      { 
+        value: "10", 
+        question: "近两周来，咳嗽会影响您的睡眠吗？", 
+        answer: [
+          {value: "1", title: "A", name: "一直都会"},
+          {value: "2", title: "B", name: "大多数时间会"},
+          {value: "3", title: "C", name: "常常会"},
+          {value: "4", title: "D", name: "有时会"},
+          {value: "5", title: "E", name: "很少会"},
+          {value: "6", title: "F", name: "几乎不会"},
+          {value: "7", title: "G", name: "一点也不会"}
+        ],
+        chosen: ""
+      },
+      {
+        value: "11", 
+        question: "近两周来，您每天阵发性咳嗽发作多吗？", 
+        answer: [ 
+          {value: "1", title: "A", name: "持续有"},
+          {value: "2", title: "B", name: "次数多"},
+          {value: "3", title: "C", name: "时时有"},
+          {value: "4", title: "D", name: "有一些"},
+          {value: "5", title: "E", name: "偶尔有"},
+          {value: "6", title: "F", name: "极少有"},
+          {value: "7", title: "G", name: "一点也没有"}
+        ],
+        chosen: ""
+      },
+      {
+        value: "12", 
+        question: "近两周来，您会因咳嗽而情绪低落吗？", 
+        answer: [ 
+          {value: "1", title: "A", name: "一直都会"},
+          {value: "2", title: "B", name: "大多数时间会"},
+          {value: "3", title: "C", name: "时常会"},
+          {value: "4", title: "D", name: "有时会"},
+          {value: "5", title: "E", name: "很少会"},
+          {value: "6", title: "F", name: "几乎不会"},
+          {value: "7", title: "G", name: "一点也不会"}
+        ],
+        chosen: ""
+      },
+      {
+        value: "13", 
+        question: "近两周来，咳嗽会让您厌烦吗？", 
+        answer: [ 
+          {value: "1", title: "A", name: "一直都会"},
+          {value: "2", title: "B", name: "大多数时间会"},
+          {value: "3", title: "C", name: "时常会"},
+          {value: "4", title: "D", name: "有时会"},
+          {value: "5", title: "E", name: "很少会"},
+          {value: "6", title: "F", name: "几乎不会"},
+          {value: "7", title: "G", name: "一点也不会"}
+        ],
+        chosen: ""
+      },
+      {
+        value: "14", 
+        question: "近两周来，咳嗽会让您声音嘶哑吗？", 
+        answer: [ 
+          {value: "1", title: "A", name: "一直都会"},
+          {value: "2", title: "B", name: "大多数时间会"},
+          {value: "3", title: "C", name: "时常会"},
+          {value: "4", title: "D", name: "有时会"},
+          {value: "5", title: "E", name: "很少会"},
+          {value: "6", title: "F", name: "几乎不会"},
+          {value: "7", title: "G", name: "一点也不会"}
+        ],
+        chosen: ""
+      },
+      {
+        value: "15", 
+        question: "近两周来，您会觉得精力充沛吗？", 
+        answer: [ 
+          {value: "1", title: "A", name: "一点也不会"},
+          {value: "2", title: "B", name: "几乎不会"},
+          {value: "3", title: "C", name: "很少会"},
+          {value: "4", title: "D", name: "有时会"},
+          {value: "5", title: "E", name: "常常会"},
+          {value: "6", title: "F", name: "多数时间会"},
+          {value: "7", title: "G", name: "一直都会"}
+        ],
+        chosen: ""
+      },
+      {
+        value: "16", 
+        question: "近两周来，咳嗽会让您担心有可能得了重病吗？", 
+        answer: [ 
+          {value: "1", title: "A", name: "一直都会"},
+          {value: "2", title: "B", name: "大多数时间会"},
+          {value: "3", title: "C", name: "时常会"},
+          {value: "4", title: "D", name: "有时会"},
+          {value: "5", title: "E", name: "很少会"},
+          {value: "6", title: "F", name: "几乎不会"},
+          {value: "7", title: "G", name: "一点也不会"}
+        ],
+        chosen: ""
+      },
+      {
+        value: "17", 
+        question: "近两周来，咳嗽会让您担心别人觉得您身体不对劲吗？", 
+        answer: [ 
+          {value: "1", title: "A", name: "一直都会"},
+          {value: "2", title: "B", name: "大多数时间会"},
+          {value: "3", title: "C", name: "时常会"},
+          {value: "4", title: "D", name: "有时会"},
+          {value: "5", title: "E", name: "很少会"},
+          {value: "6", title: "F", name: "几乎不会"},
+          {value: "7", title: "G", name: "一点也不会"}
+        ],
+        chosen: ""
+      },
+      {
+        value: "18", 
+        question: "近两周来，您会因咳嗽中断谈话或接听电话吗？", 
+        answer: [ 
+          {value: "1", title: "A", name: "每次都会"},
+          {value: "2", title: "B", name: "大多数时间会"},
+          {value: "3", title: "C", name: "时常会"},
+          {value: "4", title: "D", name: "有时会"},
+          {value: "5", title: "E", name: "很少会"},
+          {value: "6", title: "F", name: "几乎不会"},
+          {value: "7", title: "G", name: "一点也不会"}
+        ],
+        chosen: ""
+      },
+      {
+        value: "19", 
+        question: "近两周来，您会觉得咳嗽惹恼了同伴﹑家人或朋友？", 
+        answer: [ 
+          {value: "1", title: "A", name: "每次都会"},
+          {value: "2", title: "B", name: "多数时间会"},
+          {value: "3", title: "C", name: "不时会"},
+          {value: "4", title: "D", name: "有时会"},
+          {value: "5", title: "E", name: "偶尔会"},
+          {value: "6", title: "F", name: "极少会"},
+          {value: "7", title: "G", name: "从来不会"}
+        ],
+        chosen: ""
+      },
+    ],
+    previous: "none",
+    submit: "none"
+  },
+  stopTouchMove: function() {
+    return false;
   },
   changeQuest: function(event) {
-    this.setData({
-      current : event.detail.current + 1
-    })
+    // this.setData({
+    //   current : event.detail.current + 1
+    // })
+    if (this.data.current === 18) {
+      this.setData({
+        submit: "block"
+      })
+    } else {
+      this.setData({
+        submit: "none"
+      })
+    }
+    if (this.data.current === 0) {
+      this.setData({
+        previous: "none"
+      })
+    } else {
+      this.setData({
+        previous: "block"
+      })
+    }
   },
   changeTab: function(event) {
-    const arr = this.data.items
-    
-    for (let i = 0; i < arr.length; ++i) {
-      const answer = arr[i].answer
-      for (let j = 0; j < answer.length; ++j) {
-        if (answer[j].value === event.currentTarget.dataset.value) {
-          answer[j].selected = "selected"
-          // this.data.chosenNum = arr[i].value
-        } else {
-          answer[j].selected = ""
-        }
+    const currentItem = this.data.items[this.data.current]
+    const answers = currentItem.answer
+    for(let i = 0; i < answers.length; i++) {
+      if (answers[i].value === event.currentTarget.dataset.value) {
+        answers[i].selected = "selected"
+        currentItem.chosen = event.currentTarget.dataset.value
+      } else {
+        answers[i].selected = ""
       }
-      
     }
-    // this.data.disabled = false
-    console.log(this.data.items)
     this.setData({
-      items: arr,
-      // chosenNum: this.data.chosenNum,
-      // disabled: this.data.disabled
+      items: this.data.items,
+      current: this.data.current < 18 ? this.data.current+1 : 18
+    })
+  },
+  goBack: function(event) {
+    this.setData({
+      current: this.data.current - 1,
+      items: this.data.items
     })
   }
 })
