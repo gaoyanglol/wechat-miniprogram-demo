7fde0e560a72a1a09c4950f8eea699f9aa2eb626
gaoyang <gaoyanglol@hotmail.com>
2021-05-15 00:32:14 +0800

    temp data

--- pages/lungFpg/lungFpg.js
+++ pages/lungFpg/lungFpg.js
@@ -5,10 +5,31 @@ var debounce = require('../../utils/debounce.js')
 
 Page({
   data: {
+    fpg_data: "",
+    breakfast_data: "",
+    lunch_data: "",
+    dinner_data: ""
+  },
 
+  onShow: function(event) {
+    this.setData({
+      fpg_data: wx.getStorageSync('fpg_temp'),
+      breakfast_data: wx.getStorageSync('breakfast_temp'),
+      lunch_data: wx.getStorageSync('lunch_temp'),
+      dinner_data: wx.getStorageSync('dinner_temp')
+    })
   },
 
   fpgStorage: debounce.debounce(function (e){
-    console.log(e[0].detail.value)
+    wx.setStorageSync('fpg_temp', e[0].detail.value)
+  }, 1000),
+  breakfastStorage: debounce.debounce(function (e){
+    wx.setStorageSync('breakfast_temp', e[0].detail.value)
+  }, 1000),
+  lunchStorage: debounce.debounce(function (e){
+    wx.setStorageSync('lunch_temp', e[0].detail.value)
+  }, 1000),
+  dinnerStorage: debounce.debounce(function (e){
+    wx.setStorageSync('dinner_temp', e[0].detail.value)
   }, 1000)
 })
